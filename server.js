77ab2e7169a0e85029101463e4bb841fe0f773fa
23120308 <>
2026-01-08 17:15:55 +0700

    Test CI CD

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello DevOps');
+  res.end('Hello DevOps Truong Thai Nguyen HCMUS');
 });
 
 server.listen(PORT, () => {
